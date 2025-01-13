d53c1db976241650e974ead4b9454e0c28ebcf58
Nikitha-098 <kinnyyyy1234@gmail.com>
2025-01-13 20:13:31 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,7 @@ echo "Please enter your username:"  #text entered here will be added as value to
 read -s USERNAME   #Here USERNAME is variable name
 
 echo "username entered: $USERNAME"
+
+echo "Please enter the password:"
+
+read -s PASSWORD
