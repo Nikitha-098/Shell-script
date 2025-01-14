eb768abfb87ccbcf577b32b375755f5240f79d20
Nikitha-098 <kinnyyyy1234@gmail.com>
2025-01-14 21:52:45 +0530

    shell script

--- 13-logs.sh
+++ 13-logs.sh
@@ -8,7 +8,7 @@ Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER-$LOGS_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ] 
