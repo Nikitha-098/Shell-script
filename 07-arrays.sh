4c59d7edc7dae10179b15507d1d480d1ab6dd4a2
Nikitha-098 <kinnyyyy1234@gmail.com>
2025-01-14 13:46:17 +0530

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,7 +4,7 @@ MOVIES=("pushpa" "rrr" "devara")
 #index starts from 00,size is 0
 
 echo "First movie: ${MOVIES[0]}"
-echo "First movie: ${MOVIES[1]}"
-echo "First movie: ${MOVIES[2]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
 
 echo "All movies are: ${MOVIES[@]}"
